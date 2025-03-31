2527dc54d37ace392a67fd88a1414eae1f040eeb
Trevor22 <kasasatrevor25@gmail.com>
2025-03-31 09:34:30 +0300

    structure setup and configurations

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name= 'Generative AI project',
+    version= '0.0.0',
+    author= ' Kasasa Trevor',
+    author_email= ' kasasatrevor25@gmail.com',
+    packages= find_packages(),
+    intall_requires= []
+
+)
